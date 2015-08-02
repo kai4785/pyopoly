584f4e515c190d14e2871dbbe95b6fd40d10dd92
Chris <crogersdev@gmail.com>
2015-08-02 09:44:51 -0400

    more merge edits

--- pyopoly/__init__.py
+++ pyopoly/__init__.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 __all__ = [
     'dice.py',
-    'space.py'
+    'space.py',
+    'property.py'
+
     ]
 
 # vim: ts=4:sw=4:expandtab
-=======
-__all__ = ['dice.py', 'property.py']
->>>>>>> develop
