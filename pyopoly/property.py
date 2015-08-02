912c6f9a440e7c1a145027ce1b2b3c57d5e79260
Kai Meyer <kai.meyer@gnukai.com>
2015-08-01 19:11:54 -0600

    Adding house and railroad support to properties.

--- pyopoly/property.py
+++ pyopoly/property.py
@@ -1,14 +1,17 @@
 '''
 property.py
 
-Can haz howse?
+I can haz howse?
 '''
 
+class LandException(Exception):
+    pass
+
 class Land(object):
-    owner = None
-    price = 0
-    mortgaged = False
     def __init__(self, name, price, *args):
+        self.owner = None
+        self.mortgaged = False
+        self.pgroup = None
         self.name = name
         self.price = price
     def is_owned():
@@ -16,6 +19,8 @@ class Land(object):
     def mortgage():
         self.mortgaged = True
         return self.price / 2
+    def set_owner(self, owner):
+        self.owner = owner
     def unmortgage():
         self.mortgaged = False
     def __str__(self):
@@ -24,32 +29,31 @@ class Land(object):
         return str(self)
 
 class Property(Land):
-    name = ""
-    owner = None
-    homes = 0
-    home_values = []
-    def __init__(self, name, price, home_values, *args):
+    def __init__(self, name, price, house_values, *args):
         super(Property, self).__init__(name, price, *args)
-        self.home_values = home_values
-    def set_owner(self, owner):
-        self.owner = owner
+        self.houses = 0
+        self.house_values = []
+        self.house_values = house_values
     def charge_rent(self, player):
         if self.owner == player or self.mortgaged == True:
-            return 0
-        rent = self.home_values[self.homes]
+            rent = 0
+        elif self.houses == 0 and self.pgroup.all_owned(self.owner):
+            rent = self.house_values[0] * 2
+        else:
+            rent = self.house_values[self.houses]
         return rent
-    def buy_home(self, count=1):
-        if self.homes == 5:
-            raise # TODO: Add custom "too many homes" exception
+    def buy_house(self, count=1):
+        if self.houses + count > 5:
+            raise LandException("Limit 5 houses. %d already owned, %d requested to purchase" % (self.houses, count)) # TODO: Add custom "too many houses" exception
+        if self.owner is None:
+            raise LandException("There is no owner, can't buy a house.") # TODO: Add custom "no owner" exception
+        self.houses += count
+    def sell_house(self, count=1):
+        if self.houses - count < 0:
+            raise LandException("Cannot sell more houses than you own. %d owned, %d requested to sell" % (self.houses, count)) # TODO: Add custom "not enough houses" exception
         if self.owner == None:
-            raise # TODO: Add custom "no owner" exception
-        self.homes += 1
-    def sell_home(self, count=1):
-        if self.homes == 0:
-            raise # TODO: Add custom "not enough homes" exception
-        if self.owner == None:
-            raise # TODO: Add custom "no owner" exception
-        self.homes -= 1
+            raise LandException("There is no owner, can't sell a house.") # TODO: Add custom "no owner" exception
+        self.houses -= count
 
 class Utility(Land):
     def __init__(self, name, price, *args):
@@ -58,15 +62,28 @@ class Utility(Land):
 class Railroad(Land):
     def __init__(self, name, price, *args):
         super(Railroad, self).__init__(name, price, *args)
+    def charge_rent(self, player):
+        rent = 0
+        prop_owned = 0
+        if self.owner == player or self.mortgaged == True:
+            rent = 0
+        else:
+            prop_owned = self.pgroup.player_owned(self.owner)
+            rent = 25 * ( 2**(prop_owned - 1) )
+        return rent
 
 class PropertyGroup(object):
-    properties = []
-    name = ""
     def __init__(self, name, *args):
-        self.properties = args
+        self.name = ""
+        self.properties = []
+        for arg in args:
+            arg.pgroup = self
+            self.properties.append(arg)
         self.name = name
+    def player_owned(self, player):
+        return len([p for p in self.properties if p.owner == player])
     def all_owned(self, player):
-        return len([p for p in self.properties if p.owner == player]) == len(self.properties)
+        return self.player_owned(player) == len(self.properties)
     def __str__(self):
         return "%s (%s)" % (self.name, self.properties)
     def __repr__(self):
--- pyopoly/test_property.py
+++ pyopoly/test_property.py
@@ -1,8 +1,14 @@
-from property import Property,Utility,Railroad,PropertyGroup
+from property import Property,Utility,Railroad,PropertyGroup,LandException
 
 # TODO: These should be player classes
 player1 = "1"
 player2 = "2"
+
+class Board(object):
+    def __init__(self):
+        self.init = False
+
+board = Board()
 
 def setup_purple():
     med = Property("Mediteranian Ave.", 60, [2, 10, 30, 90, 160, 250])
@@ -33,78 +39,146 @@ def setup_utilities():
 
     return electric, water, utilities
 
-def test_owner_type():
-    assert type(player1) != str
+def setup_board():
+    if not board.init:
+        board.init = True
+        board.med, board.baltic, board.purple = setup_purple()
+        board.ori, board.verm, board.conn, board.lightblue = setup_lightblue()
+        board.rr, board.pr, board.bo, board.sl, board.railroads = setup_railroad()
+        board.electric, board.water, board.utilities = setup_utilities()
+    return board
 
 def test_property():
-    med, baltic, purple = setup_purple()
+    board = setup_board()
 
-    assert med in purple.properties
-    assert baltic in purple.properties
-    assert len(purple.properties) == 2
+    assert board.med in board.purple.properties
+    assert board.baltic in board.purple.properties
+    assert len(board.purple.properties) == 2
 
 def test_railroads():
-    rr, pr, bo, sl, railroads = setup_railroad()
+    board = setup_board()
 
-    assert rr in railroads.properties
-    assert pr in railroads.properties
-    assert bo in railroads.properties
-    assert sl in railroads.properties
-    assert len(railroads.properties) == 4
+    assert board.rr in board.railroads.properties
+    assert board.pr in board.railroads.properties
+    assert board.bo in board.railroads.properties
+    assert board.sl in board.railroads.properties
+    assert len(board.railroads.properties) == 4
 
 def test_utilities():
-    electric, water, utilities = setup_utilities()
+    board = setup_board()
 
-    assert water in utilities.properties
-    assert electric in utilities.properties
-    assert len(utilities.properties) == 2
+    assert board.water in board.utilities.properties
+    assert board.electric in board.utilities.properties
+    assert len(board.utilities.properties) == 2
 
 def test_group_owner():
-    med, baltic, purple = setup_purple()
-    ori, verm, conn, lightblue = setup_lightblue()
+    board = setup_board()
 
-    med.set_owner(player1)
-    baltic.set_owner(player1)
-    ori.set_owner(player1)
-    verm.set_owner(player2)
+    board.med.set_owner(player1)
+    board.baltic.set_owner(player1)
+    board.ori.set_owner(player1)
+    board.verm.set_owner(player2)
 
-    assert purple.all_owned(player1)
-    assert not purple.all_owned(player2)
+    assert board.purple.all_owned(player1)
+    assert not board.purple.all_owned(player2)
 
-    assert not lightblue.all_owned(player1)
-    assert not lightblue.all_owned(player2)
+    assert not board.lightblue.all_owned(player1)
+    assert not board.lightblue.all_owned(player2)
 
 def test_property_rent():
-    med, baltic, purple = setup_purple()
-    ori, verm, conn, lightblue = setup_lightblue()
+    board = setup_board()
 
-    med.set_owner(player1)
-    baltic.set_owner(player1)
-    ori.set_owner(player1)
-    verm.set_owner(player2)
+    board.med.set_owner(player1)
+    board.baltic.set_owner(player1)
+    board.ori.set_owner(player1)
+    board.verm.set_owner(player2)
 
-    assert med.charge_rent(player1) == 0
-    assert med.charge_rent(player2) == 2
-    assert baltic.charge_rent(player1) == 0
-    assert baltic.charge_rent(player2) == 4
-    assert ori.charge_rent(player1) == 0
-    assert ori.charge_rent(player2) == 30
-    assert verm.charge_rent(player1) == 30
-    assert verm.charge_rent(player2) == 0
+    # Player 1 owns both purple, so rent should be double
+    assert board.med.charge_rent(player1) == 0
+    assert board.med.charge_rent(player2) == 4
+    assert board.baltic.charge_rent(player1) == 0
+    assert board.baltic.charge_rent(player2) == 8
+    assert board.ori.charge_rent(player1) == 0
+    assert board.ori.charge_rent(player2) == 30
+    assert board.verm.charge_rent(player1) == 30
+    assert board.verm.charge_rent(player2) == 0
 
 def test_property_houses():
-    raise BaseException("Implement me!")
+    board = setup_board()
 
-def test_property_hotel():
-    raise BaseException("Implement me!")
+    board.med.set_owner(player1)
+    board.baltic.set_owner(player1)
 
-def test_hotel():
-    raise BaseException("Implement me!")
+    board.med.buy_house(3)
+    board.baltic.buy_house(4)
+
+    assert board.med.charge_rent(player1) == 0
+    assert board.med.charge_rent(player2) == 90
+    assert board.baltic.charge_rent(player1) == 0
+    assert board.baltic.charge_rent(player2) == 320
+
+    try:
+        board.med.buy_house(3)
+    except LandException, e:
+        pass
+    try:
+        board.med.sell_house(5)
+    except LandException, e:
+        pass
+    try:
+        board.verm.buy_house(1)
+    except LandException, e:
+        pass
+
+def test_railroad_rent():
+    board = setup_board()
+
+    #rr, pr, bo, sl
+    board.rr.set_owner(player1)
+
+    assert board.rr.charge_rent(player1) == 0
+    assert board.rr.charge_rent(player2) == 25
+
+    board.pr.set_owner(player1)
+
+    assert board.rr.charge_rent(player1) == 0
+    assert board.rr.charge_rent(player2) == 50
+    assert board.pr.charge_rent(player1) == 0
+    assert board.pr.charge_rent(player2) == 50
+
+    board.bo.set_owner(player2)
+    assert board.rr.charge_rent(player1) == 0
+    assert board.rr.charge_rent(player2) == 50
+    assert board.pr.charge_rent(player1) == 0
+    assert board.pr.charge_rent(player2) == 50
+    assert board.bo.charge_rent(player1) == 25
+    assert board.bo.charge_rent(player2) == 0
+
+    board.sl.set_owner(player1)
+    assert board.rr.charge_rent(player1) == 0
+    assert board.rr.charge_rent(player2) == 100
+    assert board.pr.charge_rent(player1) == 0
+    assert board.pr.charge_rent(player2) == 100
+    assert board.bo.charge_rent(player1) == 25
+    assert board.bo.charge_rent(player2) == 0
+    assert board.sl.charge_rent(player1) == 0
+    assert board.sl.charge_rent(player2) == 100
+
+    board.bo.set_owner(player1)
+    assert board.rr.charge_rent(player1) == 0
+    assert board.rr.charge_rent(player2) == 200
+    assert board.pr.charge_rent(player1) == 0
+    assert board.pr.charge_rent(player2) == 200
+    assert board.bo.charge_rent(player1) == 0
+    assert board.bo.charge_rent(player2) == 200
+    assert board.sl.charge_rent(player1) == 0
+    assert board.sl.charge_rent(player2) == 200
 
 def test_utility_rent():
     raise BaseException("Implement me!")
 
-def test_railroad_rent():
-    raise BaseException("Implement me!")
+def test_owner_type():
+    assert type(player1) != str
+
 
 # vim: ts=4:sw=4:expandtab
